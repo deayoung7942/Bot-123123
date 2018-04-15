ec4c5947750833d02397e62a9d0190ad029ec1cb
deayoung7942 <38397901+deayoung7942@users.noreply.github.com>
2018-04-15 20:39:52 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,13 @@ client.on('message', message => {
     message.reply(message.author.avatarURL);
   }
 });
-
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
  
 client.login(process.env.BOT_TOKEN);
