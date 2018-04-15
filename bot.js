4bad243a51c6de880e2909a90aad8f815efff407
deayoung7942 <38397901+deayoung7942@users.noreply.github.com>
2018-04-15 21:01:22 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,9 @@ client.on("ready", () => {
 client.on("message", message => {
   if (message.content == "ping") {
     message.reply("pong");
+   
+  if (message.content == "ping2") {
+    message.channel.send("pong2");
   }
   // If the message is "what is my avatar"
   if (message.content === 'what is my avatar') {
